800d5442ef18d65f54053fc657b98fe6f6dcda76
aslivets <slivets.aleksay@gmail.com>
2016-03-23 23:37:21 +0100

    days of week example

---
+++ DaysOfWeek.java
@@ -0,0 +1,19 @@
+public enum DaysOfWeek {
+    SUNDAY("Неділя"),
+    MONDAY("Понеділок"),
+    TUESDAY("Вівторок"),
+    WEDNESDAY("Середа"),
+    THURSDAY("Четвер"),
+    FRIDAY("П'ятниця"),
+    SATURDAY("Субота");
+
+    private String ukrName;
+    DaysOfWeek(String dayName) {
+        this.ukrName = dayName;
+    }
+
+    public void printDay(){
+        System.out.println(ukrName);
+    }
+
+}
---
+++ PrintWeek.java
@@ -0,0 +1,7 @@
+public class PrintWeek {
+    public void printDays() {
+        for (DaysOfWeek day : DaysOfWeek.values()) {
+            day.printDay();
+        }
+    }
+}
